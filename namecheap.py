56cd3e7e9eb9f1c161af629c7d75defd0c4bdb8d
anfractuosity <anfractuosity@anfractuosity.com>
2020-01-20 22:01:22 +0000

    Fix TLD issue

--- namecheap.py
+++ namecheap.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import requests  # pip install requests
+import tldextract
 from xml.etree.ElementTree import fromstring
 
 inPy3k = sys.version_info[0] == 3
@@ -11,8 +12,8 @@ ENDPOINTS = {
     # 1) browse to http://www.sandbox.namecheap.com/
     # 2) create account, my account > manage profile > api access > enable API
     # 3) add your IP address to whitelist
-    'sandbox': 'https://api.sandbox.namecheap.com/xml.response',
-    'production': 'https://api.namecheap.com/xml.response',
+    "sandbox": "https://api.sandbox.namecheap.com/xml.response",
+    "production": "https://api.namecheap.com/xml.response",
 }
 NAMESPACE = "http://api.namecheap.com/xml.response"
 
@@ -24,22 +25,29 @@ DEFAULT_ATTEMPTS_DELAY = 0.1  # in seconds
 # https://www.namecheap.com/support/api/error-codes.aspx
 class ApiError(Exception):
     def __init__(self, number, text):
-        Exception.__init__(self, '%s - %s' % (number, text))
+        Exception.__init__(self, "%s - %s" % (number, text))
         self.number = number
         self.text = text
 
 
 class Api(object):
     # Follows API spec capitalization in variable names for consistency.
-    def __init__(self, ApiUser, ApiKey, UserName, ClientIP,
-                 sandbox=True, debug=True,
-                 attempts_count=DEFAULT_ATTEMPTS_COUNT,
-                 attempts_delay=DEFAULT_ATTEMPTS_DELAY):
+    def __init__(
+        self,
+        ApiUser,
+        ApiKey,
+        UserName,
+        ClientIP,
+        sandbox=True,
+        debug=True,
+        attempts_count=DEFAULT_ATTEMPTS_COUNT,
+        attempts_delay=DEFAULT_ATTEMPTS_DELAY,
+    ):
         self.ApiUser = ApiUser
         self.ApiKey = ApiKey
         self.UserName = UserName
         self.ClientIP = ClientIP
-        self.endpoint = ENDPOINTS['sandbox' if sandbox else 'production']
+        self.endpoint = ENDPOINTS["sandbox" if sandbox else "production"]
         self.debug = debug
         self.payload_limit = 10  # After hitting this lenght limit script will move payload from POST params to POST data
         self.attempts_count = attempts_count
@@ -48,9 +56,19 @@ class Api(object):
     # https://www.namecheap.com/support/api/methods/domains/create.aspx
     def domains_create(
         self,
-        DomainName, FirstName, LastName,
-        Address1, City, StateProvince, PostalCode, Country, Phone,
-        EmailAddress, Address2=None, years=1, WhoisGuard=False
+        DomainName,
+        FirstName,
+        LastName,
+        Address1,
+        City,
+        StateProvince,
+        PostalCode,
+        Country,
+        Phone,
+        EmailAddress,
+        Address2=None,
+        years=1,
+        WhoisGuard=False,
     ):
         """
         Registers a domain name with the given contact info.
@@ -58,44 +76,42 @@ class Api(object):
 
         For simplicity assumes one person acts as all contact types."""
 
-        contact_types = ['Registrant', 'Tech', 'Admin', 'AuxBilling']
+        contact_types = ["Registrant", "Tech", "Admin", "AuxBilling"]
 
-        extra_payload = {
-            'DomainName': DomainName,
-            'years': years
-        }
+        extra_payload = {"DomainName": DomainName, "years": years}
 
         if WhoisGuard:
-            extra_payload.update({
-                'AddFreeWhoisguard': 'yes',
-                'WGEnabled': 'yes',
-            })
+            extra_payload.update(
+                {"AddFreeWhoisguard": "yes", "WGEnabled": "yes",}
+            )
 
         for contact_type in contact_types:
-            extra_payload.update({
-                '%sFirstName' % contact_type: FirstName,
-                '%sLastName' % contact_type: LastName,
-                '%sAddress1' % contact_type: Address1,
-                '%sCity' % contact_type: City,
-                '%sStateProvince' % contact_type: StateProvince,
-                '%sPostalCode' % contact_type: PostalCode,
-                '%sCountry' % contact_type: Country,
-                '%sPhone' % contact_type: Phone,
-                '%sEmailAddress' % contact_type: EmailAddress,
-            })
+            extra_payload.update(
+                {
+                    "%sFirstName" % contact_type: FirstName,
+                    "%sLastName" % contact_type: LastName,
+                    "%sAddress1" % contact_type: Address1,
+                    "%sCity" % contact_type: City,
+                    "%sStateProvince" % contact_type: StateProvince,
+                    "%sPostalCode" % contact_type: PostalCode,
+                    "%sCountry" % contact_type: Country,
+                    "%sPhone" % contact_type: Phone,
+                    "%sEmailAddress" % contact_type: EmailAddress,
+                }
+            )
             if Address2:
-                extra_payload['%sAddress2' % contact_type] = Address2
+                extra_payload["%sAddress2" % contact_type] = Address2
 
-        self._call('namecheap.domains.create', extra_payload)
+        self._call("namecheap.domains.create", extra_payload)
 
     def _payload(self, Command, extra_payload={}):
         """Make dictionary for passing to requests.post"""
         payload = {
-            'ApiUser': self.ApiUser,
-            'ApiKey': self.ApiKey,
-            'UserName': self.UserName,
-            'ClientIP': self.ClientIP,
-            'Command': Command,
+            "ApiUser": self.ApiUser,
+            "ApiKey": self.ApiKey,
+            "UserName": self.UserName,
+            "ClientIP": self.ClientIP,
+            "Command": Command,
         }
         # Namecheap recommends to use HTTPPOST method when setting more than 10 hostnames
         # https://www.namecheap.com/support/api/methods/domains-dns/set-hosts.aspx
@@ -104,7 +120,7 @@ class Api(object):
             extra_payload = {}
         return payload, extra_payload
 
-    def _fetch_xml(self, payload, extra_payload = None):
+    def _fetch_xml(self, payload, extra_payload=None):
         """Make network call and return parsed XML element"""
         attempts_left = self.attempts_count
         while attempts_left > 0:
@@ -116,9 +132,9 @@ class Api(object):
                 break
             if attempts_left <= 1:
                 # Here we provide 1 error code which is not present in official docs
-                raise ApiError('1', 'Did not receive 200 (Ok) response')
+                raise ApiError("1", "Did not receive 200 (Ok) response")
             if self.debug:
-                print('Received status %d ... retrying ...' % (r.status_code))
+                print("Received status %d ... retrying ..." % (r.status_code))
             time.sleep(self.attempts_delay)
             attempts_left -= 1
 
@@ -130,11 +146,11 @@ class Api(object):
             print(r.text)
         xml = fromstring(r.text)
 
-        if xml.attrib['Status'] == 'ERROR':
+        if xml.attrib["Status"] == "ERROR":
             # Response namespace must be prepended to tag names.
-            xpath = './/{%(ns)s}Errors/{%(ns)s}Error' % {'ns': NAMESPACE}
+            xpath = ".//{%(ns)s}Errors/{%(ns)s}Error" % {"ns": NAMESPACE}
             error = xml.find(xpath)
-            raise ApiError(error.attrib['Number'], error.text)
+            raise ApiError(error.attrib["Number"], error.text)
 
         return xml
 
@@ -148,13 +164,17 @@ class Api(object):
         """When listing domain names, only one page is returned
         initially. The list needs to be paged through to see all.
         This iterator gets the next page when necessary."""
+
         def _get_more_results(self):
             xml = self.api._fetch_xml(self.payload)
-            xpath = './/{%(ns)s}CommandResponse/{%(ns)s}DomainGetListResult/{%(ns)s}Domain' % {'ns': NAMESPACE}
+            xpath = (
+                ".//{%(ns)s}CommandResponse/{%(ns)s}DomainGetListResult/{%(ns)s}Domain"
+                % {"ns": NAMESPACE}
+            )
             domains = xml.findall(xpath)
             for domain in domains:
                 self.results.append(domain.attrib)
-            self.payload['Page'] += 1
+            self.payload["Page"] += 1
 
         def __init__(self, api, payload):
             self.api = api
@@ -174,15 +194,15 @@ class Api(object):
                 raise StopIteration
             else:
                 return self.results[self.i]
+
         next = __next__
 
     # https://www.namecheap.com/support/api/methods/domains-dns/set-default.aspx
     def domains_dns_setDefault(self, domain):
-        sld, tld = domain.split(".")
-        self._call("namecheap.domains.dns.setDefault", {
-            'SLD': sld,
-            'TLD': tld
-        })
+        ex = tldextract.extract(domain)
+        sld = ex.domain
+        tld = ex.suffix
+        self._call("namecheap.domains.dns.setDefault", {"SLD": sld, "TLD": tld})
 
     # https://www.namecheap.com/support/api/methods/domains/check.aspx
     def domains_check(self, domains):
@@ -206,12 +226,16 @@ class Api(object):
             if isinstance(domains, str):
                 return list(self.domains_check([domains]).items())[0][1]
 
-        extra_payload = {'DomainList': ",".join(domains)}
-        xml = self._call('namecheap.domains.check', extra_payload)
-        xpath = './/{%(ns)s}CommandResponse/{%(ns)s}DomainCheckResult' % {'ns': NAMESPACE}
+        extra_payload = {"DomainList": ",".join(domains)}
+        xml = self._call("namecheap.domains.check", extra_payload)
+        xpath = ".//{%(ns)s}CommandResponse/{%(ns)s}DomainCheckResult" % {
+            "ns": NAMESPACE
+        }
         results = {}
         for check_result in xml.findall(xpath):
-            results[check_result.attrib['Domain']] = check_result.attrib['Available'] == 'true'
+            results[check_result.attrib["Domain"]] = (
+                check_result.attrib["Available"] == "true"
+            )
         return results
 
     @classmethod
@@ -236,9 +260,12 @@ class Api(object):
             'cat3' : 'meow'
         }
         """
-        return dict(sum([
-            [(k + str(i + 1), v) for k, v in d.items()] for i, d in enumerate(l)
-        ], []))
+        return dict(
+            sum(
+                [[(k + str(i + 1), v) for k, v in d.items()] for i, d in enumerate(l)],
+                [],
+            )
+        )
 
     @classmethod
     def _elements_names_fix(self, host_record):
@@ -265,17 +292,14 @@ class Api(object):
         }
         """
 
-        conversion_map = [
-            ("Name", "HostName"),
-            ("Type", "RecordType")
-        ]
+        conversion_map = [("Name", "HostName"), ("Type", "RecordType")]
 
         for field in conversion_map:
             # if source field exists
             if field[0] in host_record:
                 # convert it to target field and delete old one
                 host_record[field[1]] = host_record[field[0]]
-                del(host_record[field[0]])
+                del host_record[field[0]]
 
         return host_record
 
@@ -291,14 +315,20 @@ class Api(object):
             ...
         }
         """
-        xml = self._call('namecheap.domains.getContacts', {'DomainName': DomainName})
-        xpath = './/{%(ns)s}CommandResponse/{%(ns)s}DomainContactsResult/*' % {'ns': NAMESPACE}
+        xml = self._call("namecheap.domains.getContacts", {"DomainName": DomainName})
+        xpath = ".//{%(ns)s}CommandResponse/{%(ns)s}DomainContactsResult/*" % {
+            "ns": NAMESPACE
+        }
         results = {}
         for contact_type in xml.findall(xpath):
             fields_for_one_contact_type = {}
-            for contact_detail in contact_type.findall('*'):
-                fields_for_one_contact_type[self._tag_without_namespace(contact_detail)] = contact_detail.text
-            results[self._tag_without_namespace(contact_type)] = fields_for_one_contact_type
+            for contact_detail in contact_type.findall("*"):
+                fields_for_one_contact_type[
+                    self._tag_without_namespace(contact_detail)
+                ] = contact_detail.text
+            results[
+                self._tag_without_namespace(contact_type)
+            ] = fields_for_one_contact_type
         return results
 
     # https://www.namecheap.com/support/api/methods/domains-dns/set-hosts.aspx
@@ -318,11 +348,10 @@ class Api(object):
         ])"""
 
         extra_payload = self._list_of_dictionaries_to_numbered_payload(host_records)
-        sld, tld = domain.split(".")
-        extra_payload.update({
-            'SLD': sld,
-            'TLD': tld
-        })
+        ex = tldextract.extract(domain)
+        sld = ex.domain
+        tld = ex.suffix
+        extra_payload.update({"SLD": sld, "TLD": tld})
         self._call("namecheap.domains.dns.setHosts", extra_payload)
 
     # https://www.namecheap.com/support/api/methods/domains-dns/set-custom.aspx
@@ -334,22 +363,25 @@ class Api(object):
         api.domains_dns_setCustom('example.com', { 'Nameservers' : 'ns1.example.com,ns2.example.com' })"""
 
         extra_payload = host_records
-        sld, tld = domain.split(".")
-        extra_payload['SLD'] = sld
-        extra_payload['TLD'] = tld
+        ex = tldextract.extract(domain)
+        sld = ex.domain
+        tld = ex.suffix
+        extra_payload["SLD"] = sld
+        extra_payload["TLD"] = tld
         self._call("namecheap.domains.dns.setCustom", extra_payload)
 
     # https://www.namecheap.com/support/api/methods/domains-dns/get-hosts.aspx
     def domains_dns_getHosts(self, domain):
         """Retrieves DNS host record settings. Note that the key names are different from those
         you use when setting the host records."""
-        sld, tld = domain.split(".")
-        extra_payload = {
-            'SLD': sld,
-            'TLD': tld
-        }
+        ex = tldextract.extract(domain)
+        sld = ex.domain
+        tld = ex.suffix
+        extra_payload = {"SLD": sld, "TLD": tld}
         xml = self._call("namecheap.domains.dns.getHosts", extra_payload)
-        xpath = './/{%(ns)s}CommandResponse/{%(ns)s}DomainDNSGetHostsResult/*' % {'ns': NAMESPACE}
+        xpath = ".//{%(ns)s}CommandResponse/{%(ns)s}DomainDNSGetHostsResult/*" % {
+            "ns": NAMESPACE
+        }
         results = []
         for host in xml.findall(xpath):
             results.append(host.attrib)
@@ -379,12 +411,13 @@ class Api(object):
 
         print("To set: %i" % len(host_records_remote))
 
-        extra_payload = self._list_of_dictionaries_to_numbered_payload(host_records_remote)
-        sld, tld = domain.split(".")
-        extra_payload.update({
-            'SLD': sld,
-            'TLD': tld
-        })
+        extra_payload = self._list_of_dictionaries_to_numbered_payload(
+            host_records_remote
+        )
+        ex = tldextract.extract(domain)
+        sld = ex.domain
+        tld = ex.suffix
+        extra_payload.update({"SLD": sld, "TLD": tld})
         self._call("namecheap.domains.dns.setHosts", extra_payload)
 
     def domains_dns_delHost(self, domain, host_record):
@@ -423,23 +456,22 @@ class Api(object):
         # Check that we delete not more than 1 record at a time
         if len(host_records_remote) != len(host_records_new) + 1:
             sys.stderr.write(
-                "Something went wrong while removing host record, delta > 1: %i -> %i, aborting API call.\n" % (
-                    len(host_records_remote),
-                    len(host_records_new)
-                )
+                "Something went wrong while removing host record, delta > 1: %i -> %i, aborting API call.\n"
+                % (len(host_records_remote), len(host_records_new))
             )
             return False
 
         extra_payload = self._list_of_dictionaries_to_numbered_payload(host_records_new)
-        sld, tld = domain.split(".")
-        extra_payload.update({
-            'SLD': sld,
-            'TLD': tld
-        })
+        ex = tldextract.extract(domain)
+        sld = ex.domain
+        tld = ex.suffix
+        extra_payload.update({"SLD": sld, "TLD": tld})
         self._call("namecheap.domains.dns.setHosts", extra_payload)
 
     # https://www.namecheap.com/support/api/methods/domains-dns/get-list.aspx
-    def domains_getList(self, ListType=None, SearchTerm=None, PageSize=None, SortBy=None):
+    def domains_getList(
+        self, ListType=None, SearchTerm=None, PageSize=None, SortBy=None
+    ):
         """Returns an iterable of dicts. Each dict represents one
         domain name the user has registered, for example
         {
@@ -458,14 +490,16 @@ class Api(object):
         # The payload is a dict of GET args that is passed to
         # the lazy-loading iterator so that it can know how to
         # get more results.
-        extra_payload = {'Page': 1}
+        extra_payload = {"Page": 1}
         if ListType:
-            extra_payload['ListType'] = ListType
+            extra_payload["ListType"] = ListType
         if SearchTerm:
-            extra_payload['SearchTerm'] = SearchTerm
+            extra_payload["SearchTerm"] = SearchTerm
         if PageSize:
-            extra_payload['PageSize'] = PageSize
+            extra_payload["PageSize"] = PageSize
         if SortBy:
-            extra_payload['SortBy'] = SortBy
-        payload, extra_payload = self._payload('namecheap.domains.getList', extra_payload)
+            extra_payload["SortBy"] = SortBy
+        payload, extra_payload = self._payload(
+            "namecheap.domains.getList", extra_payload
+        )
         return self.LazyGetListIterator(self, payload)
